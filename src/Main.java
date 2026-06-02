7bdc1bd2fa06df6ef0b4d5262ed6b2542303ebd4
Gabriel Mota <gabrielalbertomota1997@gmail.com>
2026-06-02 19:03:20 -0300

    feat:Threads Funcionando

--- src/Main.java
+++ src/Main.java
@@ -1,48 +1,51 @@
-
 import java.util.Random;
 
-public class My_threads extends Thread {
+class My_threads extends Thread {
     public static double[] vetor = new double[200_000_000];
-    public static int inicio = 0;
-    public static int fim = vetor.length;
-    public static int meio = fim/2;
 
-    public void My_threads(double[] vetor,int inicio,int fim, int meio){
-        this.vetor =vetor;
+    public int inicio;
+    public int fim;
+
+    public My_threads(int inicio, int fim) {
         this.inicio = inicio;
         this.fim = fim;
-        this.meio = meio;
-
     }
 
-    public static void main(String[] args){
-        System.out.println("Encerro a inicialização...");
+    public static void main(String[] args) {
+        int tamanho = vetor.length;
+        int meio = tamanho / 2;
 
-
-        My_threads t1 = new My_threads(vetor,inicio,meio);
-        My_threads t2 = new My_threads(vetor,meio,fim);
+        My_threads t1 = new My_threads(0, meio);
+        My_threads t2 = new My_threads(meio, tamanho);
 
         t1.start();
         t2.start();
-        try{
-            for(int i =0 ; i<fim;i++){
 
-            }
+        try {
             t1.join();
             t2.join();
-        }
-        catch(InterruptedException e){
+
+            System.out.println("Encerrou inicializacao");
+
+            int contagemTotal = 0;
+            for (double valor : vetor) {
+                if (valor > 0.25 && valor < 0.75) {
+                    contagemTotal++;
+                }
+            }
+
+            System.out.println("Quantidade de valores entre 0.25 e 0.75: " + contagemTotal);
+
+        } catch (InterruptedException e) {
             System.out.print(e);
         }
-
     }
 
-    public void run(){
+    @Override
+    public void run() {
         Random semente = new Random();
-        System.out.println("Iniciando o vetor..");
-        for (int i=0; i<vetor.length;i++){
-            double valor = semente.nextDouble();
-                vetor[i] = valor;
+        for (int i = inicio; i < fim; i++) {
+            vetor[i] = semente.nextDouble();
         }
     }
 }
