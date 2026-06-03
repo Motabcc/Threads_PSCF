1c7189a40837a38219e0ab8ad1caa7a0bb9c750b
Gabriel Mota <gabrielalbertomota1997@gmail.com>
2026-06-03 13:50:19 -0300

    feat: implementa classe Conta e threads concorrentes de saque e deposito

--- Tarefa_01_03/src/Main.java
+++ Tarefa_01_03/src/Main.java
@@ -1,58 +1,100 @@
-import java.util.random
+import java.util.Random;
+
+class Conta {
+    private  double valor;
+    private double saldo;
 
 
-class Conta extends Thread{
-    private long valor;
-
-    public Conta(long valor){
-        this.valor =valor
+    public Conta(double valor){
+        this.valor =valor;
     }
+    public void saque(double valor){
+        synchronized (this){
+            if(saldo >=valor){
+            saldo-= valor;
+            System.out.println("[SAQUE REALIZADO] SALDO ATUAL: R$"+String.format("%.2f",saldo) +" | VALOR SACADO: "+ String.format("%.2f",valor));
+            }else{
+                System.out.println("[SAQUE RECUSADO] SALDO ATUAL: R$"+String.format("%.2f",saldo) +" | TENTATIVA: "+ String.format("%.2f",valor));
 
-    public double saque(long valor){
-        return saldo-= valor;
+            }
+
+        }
     }
-
-    public void deposito(long valor){
+    public void deposito(double valor){
         synchronized(this){
          saldo += valor;
+         System.out.println("[DEPOSITO] SALDO ATUAL: "+String.format("%.2f",saldo) +" | VALOR DEPOSITADO: "+ String.format("%.2f",valor));
+
         }
     }
 
     public double saldo (){
-        return  saldo()
+        return  this.saldo;
+    }
+
+}
+
+class Threads_Saque extends Thread{
+    private  Conta contaCompartilhda;
+
+    public Threads_Saque(Conta conta){
+        this.contaCompartilhda = conta;
+    }
+    public void run(){
+
+        while(true){
+            Random semente = new Random();
+            double valor_random = semente.nextDouble() *100;
+            contaCompartilhda.saque(valor_random);
+            try {
+                Thread.sleep(100);
+
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
+
+class Threads_Deposito extends Thread{
+    private Conta contaCompartilhada;
+    public Threads_Deposito(Conta conta){
+        this.contaCompartilhada =conta;
     }
 
 
+    public void run(){
 
+        while(true){
+            Random semente = new Random();
+            double valor_random = semente.nextDouble() *100;
+            contaCompartilhada.deposito(valor_random);
+            try{
+            Thread.sleep(100);
+            } catch(InterruptedException e ){
+                e.printStackTrace();
+            }
+        }
+    }
 }
-class Threads_Saque{
-    public void main(String[] args){
 
-        Conta t1_saque =new Conta(saque(semente));
-        Conta t2_saque =new Conta(saque(semente));
-        Conta t3_saque =new Conta(saque(semente));
+class Main{
+    public static void main(String[] args){
+        Conta contaPrincipal = new Conta(0.0);
 
+        Threads_Saque t1_saque = new Threads_Saque(contaPrincipal);
+        Threads_Saque t2_saque = new Threads_Saque(contaPrincipal);
+        Threads_Saque t3_saque = new Threads_Saque(contaPrincipal);
+
+        Threads_Deposito t1_deposito =new Threads_Deposito(contaPrincipal);
+        Threads_Deposito t2_deposito =new Threads_Deposito(contaPrincipal);
 
         t1_saque.start();
         t2_saque.start();
         t3_saque.start();
 
-    }
-    public void run(){
-    }
-}
-
-class Threads_Deposito extends Threads{
-    public void main(String[] args){
-
-        Conta t1_deposito =new Conta(deposito(semente));
-        Conta t2_depostio =new Conta(deposito(semente));
-
         t1_deposito.start();
         t2_deposito.start();
 
     }
-    public void run(){
-    }
-
 }
